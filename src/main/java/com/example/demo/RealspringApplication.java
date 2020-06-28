a34ddfc4a596bc85f54d51f40f9fdb188080ab0c
harry <387551654@qq.com>
2020-06-28 10:36:55 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/RealspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealspringApplication.class, args);
+    }
+
+}
